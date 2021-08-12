bd87f6deccd4d2c306b05581bc4527e5c7d4328e
slowy07 <slowy.arfy@gmail.com>
2021-08-12 22:33:32 +0700

    update web

--- docs/script.js
+++ docs/script.js
@@ -1,105 +1,96 @@
-// ParticlesJS Config.
-particlesJS("particles-js", {
-    "particles": {
-      "number": {
-        "value": 80,
-        "density": {
-          "enable": true,
-          "value_area": 700 } },
+var fullAnimation = (function() {
   
+  var bigDarkRectangle = anime.timeline({
+    targets: '.hero-figure-box-05',
+    autoplay: false,
+    begin: function(anim) {
+      smallDarkRectangles.play()
+      colouredRectangles.play()
+    }
+  }).add({
+    duration: 400,
+    easing: 'easeInOutExpo',
+    scaleX: [0.05, 0.05],
+    scaleY: [0, 1],
+    perspective: '500px'
+  }).add({
+    duration: 400,
+    easing: 'easeInOutExpo',
+    scaleX: 1
+  }).add({
+    duration: 800,
+    rotateY: '-15deg',
+    rotateX: '8deg',
+    rotateZ: '-1deg'
+  })
+
+  var smallDarkRectangles = anime.timeline({
+    targets: '.hero-figure-box-06, .hero-figure-box-07',
+    autoplay: false
+  }).add({
+    duration: 400,
+    easing: 'easeInOutExpo',
+    scaleX: [0.05, 0.05],
+    scaleY: [0, 1],
+    perspective: '500px',
+  }).add({
+    duration: 400,
+    easing: 'easeInOutExpo',
+    scaleX: 1
+  }).add({
+    duration: 800,
+    rotateZ: '20deg'
+  })
   
-      "color": {
-        "value": "#ffffff" },
-  
-      "shape": {
-        "type": "circle",
-        "stroke": {
-          "width": 0,
-          "color": "#000000" },
-  
-        "polygon": {
-          "nb_sides": 5 } },
-  
-  
-      "opacity": {
-        "value": 0.5,
-        "random": false,
-        "anim": {
-          "enable": false,
-          "speed": 0.1,
-          "opacity_min": 0.1,
-          "sync": false } },
-  
-  
-      "size": {
-        "value": 3,
-        "random": true,
-        "anim": {
-          "enable": false,
-          "speed": 10,
-          "size_min": 0.1,
-          "sync": false } },
-  
-  
-      "line_linked": {
-        "enable": true,
-        "distance": 150,
-        "color": "#ffffff",
-        "opacity": 0.4,
-        "width": 1 },
-  
-      "move": {
-        "enable": true,
-        "speed": 2,
-        "direction": "none",
-        "random": false,
-        "straight": false,
-        "out_mode": "out",
-        "bounce": false,
-        "attract": {
-          "enable": false,
-          "rotateX": 600,
-          "rotateY": 1200 } } },
-  
-  
-  
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onhover": {
-          "enable": true,
-          "mode": "grab" },
-  
-        "onclick": {
-          "enable": true,
-          "mode": "push" },
-  
-        "resize": true },
-  
-      "modes": {
-        "grab": {
-          "distance": 140,
-          "line_linked": {
-            "opacity": 1 } },
-  
-  
-        "bubble": {
-          "distance": 400,
-          "size": 40,
-          "duration": 2,
-          "opacity": 8,
-          "speed": 3 },
-  
-        "repulse": {
-          "distance": 200,
-          "duration": 0.4 },
-  
-        "push": {
-          "particles_nb": 4 },
-  
-        "remove": {
-          "particles_nb": 2 } } },
-  
-  
-  
-    "retina_detect": true });
+  var colouredRectangles = anime.timeline({
+    targets: '.hero-figure-box-01, .hero-figure-box-02, .hero-figure-box-03, .hero-figure-box-04, .hero-figure-box-08, .hero-figure-box-09, .hero-figure-box-10',
+    autoplay: false
+  }).add({
+    duration: anime.random(600, 800),
+    delay: anime.random(600, 800),
+    rotate: [ anime.random(-360, 360), function (el) { return el.getAttribute('data-rotation') } ],
+    scale: [0.7, 1],
+    opacity: [0, 1],
+    easing: 'easeInOutExpo'
+  })   
+
+  function init() {
+    setTimeout(function() {
+      bigDarkRectangle.play()
+    }, 1000)
+  }
+
+  function restart() {
+    bigDarkRectangle.restart()
+    smallDarkRectangles.restart()
+    colouredRectangles.restart()
+  }
+
+  function seek() {
+    bigDarkRectangle.seek(bigDarkRectangle.duration * (seekProgressEl.value / 100))
+    smallDarkRectangles.seek(smallDarkRectangles.duration * (seekProgressEl.value / 100))
+    colouredRectangles.seek(colouredRectangles.duration * (seekProgressEl.value / 100))
+  }
+
+  return {
+    init: init,
+    restart: restart,
+    seek: seek
+  }
+})()
+
+// Start on load
+window.onload = function() {
+    fullAnimation.init()
+}
+
+// Re-run
+document.getElementById('run-trigger').addEventListener('click', function(e) {
+    fullAnimation.restart()
+})
+
+// Seek
+var seekProgressEl = document.querySelector('.progress');
+seekProgressEl.addEventListener('input', function() { 
+    fullAnimation.seek()
+})
